c1dc6e14ec71ac7d9fe01fc0e0fbaf984607de14
Salim Semakula <salimsema@yahoo.com>
2021-06-18 19:35:38 +0300

    nextHead

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { createClient } from "contentful";
 import RecipeCard from "../components/RecipeCard";
+import Head from "next/head";
 
 export async function getStaticProps() {
   const client = createClient({
@@ -21,10 +22,16 @@ export default function Recipes({ recipes }) {
   // console.log(recipes);
 
   return (
-    <div className="recipe-list">
-      {recipes.map((recipe) => (
-        <RecipeCard key={recipe.sys.id} recipe={recipe} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>Just Add Marmite</title>
+        <meta property="og:title" content="Just Add Marmite" key="title" />
+      </Head>
+      <div className="recipe-list">
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.sys.id} recipe={recipe} />
+        ))}
+      </div>
+    </>
   );
 }
